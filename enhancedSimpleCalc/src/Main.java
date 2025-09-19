c985c7a1f56146d3977fd53149e45d14c590b5a5
Zaki <145068764+onlyzaki@users.noreply.github.com>
2025-09-19 14:24:03 +0100

    Update Main.java

--- enhancedSimpleCalc/src/Main.java
+++ enhancedSimpleCalc/src/Main.java
@@ -7,18 +7,21 @@ public class Main {
         String numberInput;
         double[] numbers;
         int size;
-        while (true) {
+        String giveResult = "this is the result: ";
+        boolean valid = false;
+        while (!valid) {
+            while (true) {
 
 
-            System.out.print("choose an operator + | - | * | / | ^ : ");
-            operator = scanner.next().charAt(0);
-            if (operator == '+' || operator == '-' || operator == '*' || operator == '/' || operator == '^') {
-                break;
-            }else {
-                System.out.println("Invalid operator, try again:");
+                System.out.print("choose an operator + | - | * | / | ^ : ");
+                operator = scanner.next().charAt(0);
+                if (operator == '+' || operator == '-' || operator == '*' || operator == '/' || operator == '^') {
+                    break;
+                } else {
+                    System.out.println("Invalid operator, try again:");
+                }
             }
-        }
-        scanner.nextLine();
+            scanner.nextLine();
         /*
         while (true){
             System.out.print("how much numbers you wanna operate: ");
@@ -39,28 +42,71 @@ public class Main {
         }
 
          */
-        while (true){
-            System.out.print("enter the numbers you wanna operate (spaced): ");
-            numberInput = scanner.nextLine().trim().replace(',', '.');
-            String[] parts = numberInput.split("\\s+");
-            try{
-                numbers = new double[parts.length];
-                for (int i = 0; i < parts.length ; i++){
-                    numbers[i] = Double.parseDouble(parts[i]);
+            while (true) {
+                System.out.print("enter the numbers you wanna operate (spaced): ");
+                numberInput = scanner.nextLine().trim().replace(',', '.');
+                String[] parts = numberInput.split("\\s+");
+                try {
+                    numbers = new double[parts.length];
+                    for (int i = 0; i < parts.length; i++) {
+                        numbers[i] = Double.parseDouble(parts[i]);
+                    }
+                    break;
+                } catch (NumberFormatException e) {
+                    System.out.println("Invalid number format, try again!");
+                    continue;
                 }
-                break;
             }
-            catch (NumberFormatException e){
-                System.out.println("Invalid number format, try again!");
-                continue;
-            }
-        }
 
-        System.out.println("You chose operator: " + operator);
-        System.out.print("Numbers: ");
-        for (double n : numbers) {
-            System.out.print(n + " ");
+            switch (operator) {
+                case '+' -> System.out.println(giveResult + plus(numbers));
+                case '-' -> System.out.println(giveResult + minus(numbers));
+                case '*' -> System.out.println(giveResult + times(numbers));
+                case '/' -> System.out.println(giveResult + division(numbers));
+                case '^' -> System.out.println(giveResult + power(numbers));
+            }
+
         }
         scanner.close();
+    }
+    static double plus(double... numbers){
+        double result = 0;
+        for(double number : numbers){
+            result += number;
+        }
+        return result;
+    }
+    static double minus(double... numbers){
+        double result = numbers[0];
+        for(int i = 1 ; i < numbers.length; i++){
+            result -= numbers[i];
+        }
+        return result;
+    }
+    static double times(double... numbers){
+        double result = numbers[0];
+        for (int i = 1 ; i < numbers.length; i++){
+            result *= numbers[i];
+        }
+
+        return result;
+    }
+    static double division(double... numbers){
+        double result = numbers[0];
+        for (int i = 1 ; i < numbers.length; i++){
+            if (numbers[i] == 0){
+                System.out.println("Invalid number,Can't devide by zero");
+                return 0;
+            }
+            result /= numbers[i];
+        }
+        return result;
+    }
+    static double power(double... numbers){
+        double result = numbers[0];
+        for (int i = 1 ; i < numbers.length; i++){
+            result = Math.pow(result, numbers[i]);
+        }
+        return result;
     }
 }
